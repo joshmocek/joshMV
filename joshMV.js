a4a4960bf4b840eaa87226f80cc1b9bdf14bc62a
joshmocekMovista <31109203+joshmocekMovista@users.noreply.github.com>
2019-09-27 16:06:39 -0500

    Update joshMV.js

--- joshMV.js
+++ joshMV.js
@@ -1,7 +1,7 @@
 (function(global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 		typeof define === 'function' && define.amd ? define(['exports'], factory) :
-		(factory((global.R = {})));
+		(factory((global.J = {})));
 }(this, (function(exports) {
 	'use strict';
 	const R = require('ramda');
